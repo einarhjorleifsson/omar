7c5fcc7118aa9e6e0b1689c2178cfd3befa10093
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2021-07-01 17:42:34 +0000

    more on gid, sid and vid

--- R/gid.R
+++ R/gid.R
@@ -1,23 +1,47 @@
-#' Title
+#' ISSCFG codes
 #'
 #' @description International Standard Statistical Classification of Fishing Gear (ISSCFG), see http://www.fao.org/fishery/docs/DOCUMENT/cwp/handbook/annex/AnnexM2fishinggear.pdf
 #' 
 #' @param con oracle connection
 #'
 #' @return A query
+#' @export
 #' 
-std_isscfg <- function(con) {
-  tbl_mar(con, "ops$einarhj.isscfg")
+gid_isscfg <- function(con) {
+  tbl_mar(con, "ops$einarhj.gid_isscfg")
 }
+
+#' ISSCFG category table
+#'
+#' @description International Standard Statistical Classification of Fishing Gear (ISSCFG), see http://www.fao.org/fishery/docs/DOCUMENT/cwp/handbook/annex/AnnexM2fishinggear.pdf
+#' 
+#' @param con oracle connection
+#'
+#' @return A query
+#' @export
+#' 
+gid_isscfg_category <- function(con) {
+  tbl_mar(con, "ops$einarhj.gid_isscfg_category")
+}
+
 
 #' Orri gear with isscfg
 #'
 #' @param con oracle connection
 #'
 #' @return A query
+#' @export
 #'
-gid_isscfg <- function(con) {
-  tbl_mar(con, "ops$einarhj.gear_isscfg")
+gid_orri_plus <- function(con) {
+  tbl_mar(con, "ops$einarhj.gid_orri_plus") %>% 
+    dplyr::select(gid = veidarfaeri,
+                  gid2,
+                  veidarfaeri = lysing,
+                  gear = lysing_enska,
+                  gid_fi = fi_veidarfaeri,
+                  gid_lods = lods_veidarfaeri,
+                  isscfg, 
+                  everything())
 }
 
 #' orri.veidarfaeri
--- R/sid.R
+++ R/sid.R
@@ -3,6 +3,8 @@
 #' @param con oracle connection
 #'
 #' @return a query
+#' 
+#' @export
 sid_orri <- function(con) {
   tbl_mar(con, "orri.fisktegundir") %>% 
     dplyr::select(sid = tegund, 
