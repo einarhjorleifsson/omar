d89bf3888d9040864f92205f993e61dcd48e1acf
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2022-03-05 11:02:51 +0000

    use icelandic view in channel - english view not maintained

--- R/channel.R
+++ R/channel.R
@@ -12,23 +12,23 @@
 #'
 ch_station <- function(con, std = TRUE, trim = TRUE) {
   
-  q <- tbl_mar(con, "channel.station_v")
+  q <- tbl_mar(con, "channel.stod_v")
   
   if(std) {
     q <- 
       q %>% 
-      dplyr::select(stid = station_id,
-                    vid = vessel_no,
-                    date = station_date,
-                    year,
-                    lon = longitude,
-                    lat = latitude,
-                    lon2 = longitude_end,
-                    lat2 = latitude_end,
-                    z1 = bottom_depth,
-                    z2 = bottom_depth_end,
-                    st = surface_temp,
-                    bt = bottom_temp,
+      dplyr::select(stid = stod_id,
+                    vid = skip_nr,
+                    date = dags,
+                    year = ar,
+                    lon = kastad_lengd,
+                    lat = kastad_breidd,
+                    lon2 = hift_lengd,
+                    lat2 = hift_breidd,
+                    z1 = botndypi_kastad,
+                    z2 = botndypi_hift,
+                    st = yfirbordshiti,
+                    bt = botnhiti,
                     dplyr::everything())
     if(trim) {
       q <- q %>% dplyr::select(stid:bt)
@@ -53,19 +53,19 @@ ch_station <- function(con, std = TRUE, trim = TRUE) {
 #'
 ch_sample <- function(con, std = TRUE, trim = TRUE) {
   
-  q <- tbl_mar(con, "channel.sample") %>% glimpse()
+  q <- tbl_mar(con, "channel.syni_v")
   
   if(std) {
     q <- 
       q %>% 
       dplyr::select(stid = stod_id,
-                    id = sample_id,
-                    gid = load_veidarfaeri,
-                    sclass = sample_category_no,
-                    t1 = tow_start,
-                    t2 = tow_end,
-                    speed,
-                    towlength = distance,
+                    id = synis_id,
+                    gid = veidarfaeri,
+                    sclass = synaflokkur_nr,
+                    t1 = togbyrjun,
+                    t2 = togendir,
+                    speed = toghradi,
+                    towlength = toglengd,
                     dplyr::everything())
     if(trim) {
       q <- q %>% dplyr::select(stid:towlength)
