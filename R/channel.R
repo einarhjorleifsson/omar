999958b9876b3e10ca186476c767ab3968b2eee7
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2024-02-11 17:03:10 +0000

    use vocabulary in channel

--- R/channel.R
+++ R/channel.R
@@ -3,8 +3,8 @@
 #' Reads channel.stod_v. Access the same view as mar::les_stod
 #' 
 #' @param con oracle connection
-#' @param std create standardized/shortcut names (default is TRUE)
-#' @param trim trim return only key variables (default is TRUE). only operational
+#' @param std boolean (default TRUE), create standardized/shortcut names
+#' @param trim boolean (default TRUE), return only key variables. only operational
 #' if std is TRUE
 #'
 #' @return a tibble query
@@ -12,26 +12,29 @@
 #'
 ch_station <- function(con, std = TRUE, trim = TRUE) {
   
-  q <- tbl_mar(con, "channel.stod_v")
+  q <- 
+    tbl_mar(con, "channel.stod_v") |> 
+    dplyr::select(stod_id,
+                  skip_nr,
+                  dags,
+                  ar,
+                  reitur,
+                  smareitur,
+                  kastad_lengd,
+                  kastad_breidd,
+                  hift_lengd,
+                  hift_breidd,
+                  botndypi_kastad,
+                  botndypi_hift,
+                  yfirbordshiti,
+                  botnhiti,
+                  dplyr::everything())
   
   if(std) {
     q <- 
-      q %>% 
-      dplyr::select(.stid = stod_id,
-                    vid = skip_nr,
-                    date = dags,
-                    year = ar,
-                    sq = reitur,
-                    ssq = smareitur,
-                    lon = kastad_lengd,
-                    lat = kastad_breidd,
-                    lon2 = hift_lengd,
-                    lat2 = hift_breidd,
-                    z1 = botndypi_kastad,
-                    z2 = botndypi_hift,
-                    st = yfirbordshiti,
-                    bt = botnhiti,
-                    dplyr::everything())
+      q |> 
+      dplyr::rename(dplyr::any_of(vocabulary)) 
+    
     if(trim) {
       q <- q %>% dplyr::select(.stid:bt)
     }
@@ -55,24 +58,27 @@ ch_station <- function(con, std = TRUE, trim = TRUE) {
 #'
 ch_sample <- function(con, std = TRUE, trim = TRUE) {
   
-  q <- tbl_mar(con, "channel.syni_v")
+  q <- 
+    tbl_mar(con, "channel.syni_v") |> 
+    dplyr::select(stod_id,
+                  synis_id,
+                  veidarfaeri,
+                  synaflokkur_nr,
+                  tog_nr,
+                  togbyrjun,
+                  togendir,
+                  tog_nr,
+                  toghradi,
+                  toglengd,
+                  dplyr::everything())
   
   if(std) {
     q <- 
-      q %>% 
-      dplyr::select(.stid = stod_id,
-                    .id = synis_id,
-                    gid = veidarfaeri,
-                    sclass = synaflokkur_nr,
-                    tow_id = tog_nr,
-                    t1 = togbyrjun,
-                    t2 = togendir,
-                    tid = tog_nr,
-                    speed = toghradi,
-                    towlength = toglengd,
-                    dplyr::everything()) %>% 
+      q |> 
       # go metric
-      dplyr::mutate(towlength = 1852 * towlength)
+      dplyr::mutate(toglengd = 1852 * toglengd) |> 
+      dplyr::rename(dplyr::any_of(vocabulary))
+      
     if(trim) {
       q <- q %>% dplyr::select(.stid:towlength)
     }
@@ -96,15 +102,18 @@ ch_sample <- function(con, std = TRUE, trim = TRUE) {
 #'
 ch_sample_class <- function(con, std = TRUE, trim = TRUE) {
   
-  q <- tbl_mar(con, "channel.sample_category") 
+  q <- 
+    tbl_mar(con, "channel.sample_category") |> 
+    dplyr::select(sample_category_no,
+                  eng_descr,
+                  descr,
+                  dplyr::everything())
   
   if(std) {
     q <-
       q |> 
-      dplyr::select(sclass = sample_category_no,
-                  sample_class = eng_descr,
-                  synaflokkur = descr,
-                  dplyr::everything())
+      dplyr::rename(dplyr::any_of(vocabulary))
+      
     if(trim) {
       q <-
         q |> 
